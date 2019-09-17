450b1fa90b11372028aedf4bff41cd1a0d0d6148
deaviss <deaviss@gmail.com>
2019-09-17 13:08:48 +0200

    init

--- zoo_spec.js
+++
@@ -1,9 +0,0 @@
-describe("input some text in google search", function () {
-	it("to test input field and check text output", function () {
-		browser.get("https://www.protractortest.org/#/api?view=ElementFinder");
-		element(by.id("searchInput")).sendKeys("angular")
-		element(by.id("searchInput")).getText().then(text => {
-			console.log(text);
-		})
-	})
-})
